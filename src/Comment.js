b5f7751cab56514a9c7b1a3f51496ecc1199b3d1
Nadia Lee <miss.nadia.lee@gmail.com>
2019-03-08 19:49:55 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,13 @@
-// add Comment component here
+import React, { Component } from 'react';
+
+export default class ColorBox extends Component {
+  
+  render() {
+    return (
+      <div className="color-box" style={{opacity: 2}}>
+        {/* your conditional code here! */}
+      </div>
+    )
+  }
+  
+}
